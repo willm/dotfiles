8452f901e2fa5e5e1ace1de87775cd5d22ab4cb9
Will Munn <will.munn@focusrite.com>
2023-08-06 13:40:32 +0100

    Add rust formatting

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -389,76 +389,7 @@ telescope.setup({
   },
 })
 
-local util = require("formatter.util")
--- Provides the Format, FormatWrite, FormatLock, and FormatWriteLock commands
-local formatter = require("formatter")
-local prettier = require("formatter.defaults.prettierd")
-formatter.setup({
-  -- Enable or disable logging
-  logging = true,
-  -- Set the log level
-  log_level = vim.log.levels.WARN,
-  -- All formatter configurations are opt-in
-  filetype = {
-    -- Formatter configurations for filetype "lua" go here
-    -- and will be executed in order
-    javascript = { prettier },
-    typescript = { prettier },
-    terraform = {
-      function()
-        return {
-          exe = "terraform",
-          args = { "fmt", "-" },
-          stdin = true,
-        }
-      end,
-    },
-    lua = {
-      -- "formatter.filetypes.lua" defines default configurations for the
-      -- "lua" filetype
-      require("formatter.filetypes.lua").stylua,
-
-      -- You can also define your own configuration
-      function()
-        -- Supports conditional formatting
-        if util.get_current_buffer_file_name() == "special.lua" then
-          return nil
-        end
-
-        -- Full specification of configurations is down below and in Vim help
-        -- files
-        return {
-          exe = "stylua",
-          args = {
-            "--search-parent-directories",
-            "--stdin-filepath",
-            util.escape_path(util.get_current_buffer_file_path()),
-            "--",
-            "-",
-          },
-          stdin = true,
-        }
-      end,
-    },
-
-    -- Use the special "*" filetype for defining formatter configurations on
-    -- any filetype
-    ["*"] = {
-      -- "formatter.filetypes.any" defines default configurations for any
-      -- filetype
-      require("formatter.filetypes.any").remove_trailing_whitespace,
-    },
-  },
-})
-
-vim.api.nvim_create_autocmd({ "BufWritePost" }, {
-  pattern = "*",
-  callback = function()
-    vim.cmd("FormatWrite")
-  end,
-  desc = "Format on save",
-})
-
+require("formatting")
 require("remaps")
 vim.opt.expandtab = true
 vim.opt.tabstop = 2
