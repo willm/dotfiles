51b7fda3219097b2c24a20662d7e5939106abad8
Will Munn <william.munn@zenika.com>
2023-09-11 19:01:06 +0200

    A few little helpers to make using terminal buffers easier

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -427,7 +427,25 @@ vim.opt.ignorecase = true
 vim.opt_local.spell = true
 vim.opt.spelllang = "en,fr"
 vim.opt.spelloptions = "camel"
+
+-- terminal helpers
+
+-- switch to insert mode when navigating to a terminal buffer
 vim.api.nvim_create_autocmd(
-  { "TermOpen", "WinEnter term://*" },
-  { command = "startinsert" }
+  { "TermOpen", "WinEnter" },
+  { pattern = "term://*", command = "startinsert" }
 )
+
+-- open a terminal in a vertical split more easily
+vim.api.nvim_create_user_command("TermOpenVsplit", function()
+  vim.cmd("vs | term")
+end, {})
+
+vim.keymap.set("n", "<leader>t", "<cmd>TermOpenVsplit<cr>")
+
+-- open a terminal in a horizontal split more easily
+vim.api.nvim_create_user_command("TermOpenHsplit", function()
+  vim.cmd("sp | term")
+end, {})
+
+vim.keymap.set("n", "<leader>T", "<cmd>TermOpenHsplit<cr>")
